e159ef6527509ea2b1641765cdfe00dadc507b9c
fran6 <igwefran6@gmail.com>
2024-02-02 11:51:14 +0100

    Changed styles folder name to css and moved it to src, created a new folder in src called js and moved all js folder into it, added a feed to quote image with html and css, edit webpack.config.js, css, js files inorder to link them to new location

--- src/index.js
+++
@@ -1,4 +0,0 @@
-import "../styles/index.css";
-import "../styles/normalize.min.css";
-import img from "../assets/images/24eatups-team.webp"
-import img2 from "../assets/images/people-eating.png"
---
+++ src/js/index.js
@@ -0,0 +1,4 @@
+import "../css/index.css";
+import "../css/normalize.min.css";
+import img from "../../assets/images/24eatups-team.png"
+import img2 from "../../assets/images/people-eating.png"
