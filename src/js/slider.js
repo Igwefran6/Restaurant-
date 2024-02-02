ec9ab14676276ffa66d70fe37940f02a2b97fdc0
fran6 <igwefran6@gmail.com>
2024-02-02 17:35:46 +0100

    ...

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,9 +1,12 @@
 import "../css/slider.css";
 
 document.addEventListener("DOMContentLoaded", function () {
-    const dots = Array.from(document.querySelectorAll(".dot"))
-    dots.map(dot => dot.addEventListener("click", ()=>{
-      const element = document.querySelector('.slider');
-      console.log(element.offsetWidth)
-    }))
+    const dots = Array.from(document.querySelectorAll(".dot"));
+    dots.map((dot, i) =>
+        dot.addEventListener("click", event => {
+            let targetItem = `.slide-${i + 1}`;
+            const scrollToElement = document.querySelector(targetItem);
+            scrollToElement.scrollIntoView({ behavior: "smooth" });
+        })
+    );
 });
